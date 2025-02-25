a7cb6f2a6a24ec7d26d39436e447eede9881ec57
devmilad <fazelianmilad3@gmail.com>
2025-02-25 15:08:10 +0330

    add post method torob api

--- routes/torob.routes.js
+++ routes/torob.routes.js
@@ -2,8 +2,8 @@ import express from "express";
 import { index, show } from "../controllers/torob.controller.js";
 const router = express.Router();
 
-router.get("/products", index);
-router.get("/product/:slug", show);
+router.post("/products", index);
+router.post("/product/:slug", show);
 
 export default router;
 
